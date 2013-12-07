2f7206de8f1cc5c9d790c41f090383ecd70fd19a
Jeff Turcotte <jeff.turcotte@gmail.com>
2013-12-06 20:22:36 -0500

    Fixed mobile navigation focus issue

--- js/bp.js
+++ js/bp.js
@@ -1,25 +1,34 @@
 ;(function() {
-	var focusedElement = null;
-	var focus = function(evt) {
-		var className = evt.target.className;
-		if (className.match(/(^| )focusable( |$)/) && !className.match(/(^| )focused( |$)/)) {
-			focusedElement = evt.target;
-			focusedElement.className += ' focused';
-		} else {
-			if (focusedElement) {
-				focusedElement.className = focusedElement.className.replace(/(^| )focused( |$)/, ' ').trim();
-				focusedElement = null;
-			}
-		}
-	};
-	var onEvent = 'click';
-	if ('ontouchstart' in window || navigator.msMaxTouchPoints) {
-		onEvent = 'touchstart';
-	}
+	var attr = 'focus',
+		dataAttr = 'data-'+attr,
+		supportsTouch = ('ontouchstart' in window || navigator.msMaxTouchPoints),
+		trigger = supportsTouch ? 'touchstart' : 'click';
 
-	if (document.addEventListener) {
-		document.addEventListener(onEvent, focus);
-	} else {
-		document.attachEvent(onEvent, focus);
-	}
+	// ie8 uses attachEvent
+	var addEvent = function(name, listener) {
+		!!(document.addEventListener)
+			? document.addEventListener(name, listener)
+			: document.attachEvent('on'+name, listener);
+	};
+
+	// add the event listener
+	addEvent(trigger, function(e) {
+		var elem = e.target;
+
+		// move up the node tree and discover the selector
+		while (elem.attributes && !(dataAttr in elem.attributes)) {
+			elem = elem.parentNode;
+		}
+
+		if (!elem.attributes) {
+			// the Document doesn't have attributes, so
+			// we can safely close the focusable area
+			elem = document.querySelector('['+dataAttr+']');
+			elem.setAttribute(dataAttr, '');
+			return;
+		}
+
+		// we're inside the focusable area, enable it
+		elem.setAttribute(dataAttr, attr);	
+	});
 })();
