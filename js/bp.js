8a99aea0475bbcc9207c45b03840f4bfdcc52c8c
Kevin Hamer <kevin@imarc.com>
2016-03-31 13:57:59 -0400

    Switching from tabs to spaces

--- js/bp.js
+++ js/bp.js
@@ -1,38 +1,38 @@
 ;(function() {
-	var attr = 'focus',
-		dataAttr = 'data-'+attr,
-		supportsTouch = ('ontouchstart' in window || navigator.msMaxTouchPoints),
-		trigger = supportsTouch ? 'touchstart' : 'click';
+    var attr = 'focus',
+        dataAttr = 'data-'+attr,
+        supportsTouch = ('ontouchstart' in window || navigator.msMaxTouchPoints),
+        trigger = supportsTouch ? 'touchstart' : 'click';
 
-	// ie8 uses attachEvent
-	var addEvent = function(name, listener) {
-		!!(document.addEventListener)
-			? document.addEventListener(name, listener)
-			: document.attachEvent('on'+name, listener);
-	};
+    // ie8 uses attachEvent
+    var addEvent = function(name, listener) {
+        !!(document.addEventListener)
+            ? document.addEventListener(name, listener)
+            : document.attachEvent('on'+name, listener);
+    };
 
-	// add the event listener
-	addEvent(trigger, function(e) {
-		var elem = e.target;
+    // add the event listener
+    addEvent(trigger, function(e) {
+        var elem = e.target;
 
-		// move up the node tree and discover the selector
-		while (elem.attributes && !(dataAttr in elem.attributes)) {
-			elem = elem.parentNode;
-		}
+        // move up the node tree and discover the selector
+        while (elem.attributes && !(dataAttr in elem.attributes)) {
+            elem = elem.parentNode;
+        }
 
-		if (!elem.attributes) {
-			// the Document doesn't have attributes, so
-			// we can safely close the focusable area
-			elem = document.querySelector('['+dataAttr+']');
+        if (!elem.attributes) {
+            // the Document doesn't have attributes, so
+            // we can safely close the focusable area
+            elem = document.querySelector('['+dataAttr+']');
 
-			if (elem) {
-				elem.setAttribute(dataAttr, '');
-			}
+            if (elem) {
+                elem.setAttribute(dataAttr, '');
+            }
 
-			return;
-		}
+            return;
+        }
 
-		// we're inside the focusable area, enable it
-		elem.setAttribute(dataAttr, attr);	
-	});
+        // we're inside the focusable area, enable it
+        elem.setAttribute(dataAttr, attr);
+    });
 })();
