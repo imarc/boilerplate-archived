2684fe2a493b039ebb3bd8a89d1f91c1b8d38cd6
Marcel Moreau <marcel@imarc.net>
2018-03-19 11:36:36 -0400

    wip

---
+++ js/site.js
@@ -0,0 +1,8 @@
+import fontawesome from '@fortawesome/fontawesome';
+import faUser from '@fortawesome/fontawesome-free-solid/faUser';
+import faCircle from '@fortawesome/fontawesome-free-regular/faCircle';
+import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebook';
+
+fontawesome.library.add(faUser);
+fontawesome.library.add(faCircle);
+fontawesome.library.add(faFacebook);
