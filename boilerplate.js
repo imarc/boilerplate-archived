f7e0c416db73e3c33fa997ed957d32117d956c51
Kevin Hamer <kevin@imarc.com>
2019-04-18 16:32:50 -0400

    adding another underline

--- boilerplate.js
+++ boilerplate.js
@@ -6,7 +6,7 @@ const path = require("path");
 const src = path.resolve(__dirname);
 const dest = path.resolve("./");
 
-console.log(`Copying files from ${src} to ${dest}...`);
+console.log(`Copying files to ${dest}...`);
 
 let files = [
     "resources",
@@ -18,9 +18,10 @@ files.forEach(file => {
     fs.copySync(path.join(src, file), path.join(dest, file));
 });
 
-console.log("Done.\n\n");
+console.log("Done.");
 
-console.log("Remember to run npm run dev first, then either "
+console.log("Remember to run "
+    + chalk.underline("npm run dev") + " first, then either "
     + chalk.underline("npm run fractal start") + " or "
     + chalk.underline("npm run fractal build")
     + " to create your pattern library.\n");
